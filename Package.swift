ea3cdfe7c52cfb52456c626dace79e22623fdfc6
Massimiliano Ribuoli <max@ribuoli.it>
2024-01-23 16:30:41 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["FittedSheetsSwiftUI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/whirr/FittedSheets", from: "main")
+        .package(url: "https://github.com/whirr/FittedSheets", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
