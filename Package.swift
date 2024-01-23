4acbe8057dfc7bc09fd67c799ba781b1524e328d
Massimiliano Ribuoli <max@ribuoli.it>
2024-01-23 16:40:26 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "FittedSheetsSwiftUI",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        .macOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
